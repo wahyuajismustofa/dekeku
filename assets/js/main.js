9e5417bed86a7587d1c666adc398b724bfef989d
wahyuajismustofa <wahyuajismustofa333@gmail.com>
2025-08-27 03:41:54 +0700

    Auto-sync 03:41 27/08/2025

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
 import { includeElement} from "./dom/include.js";
 import { updateCartBadge } from "./utils/cart.js";
-import { dekekuFunction as _dF } from "./dekeku.js";
+import dekeku, { dekekuFunction as _dF } from "./dekeku.js";
 
 
 document.addEventListener('DOMContentLoaded', async () => {
   await includeElement();
   updateCartBadge();
-  _dF.initDekeku();
+  try{
+    await _dF.waitUntilTrue(() => typeof dekeku !== "undefined" && dekeku.ready === true);
+    dekeku.params = _dF.readURLParams();
+    dekeku.params_1 = _dF.params.getParams();
+    _dF.initDekeku();
+  }catch (err) {
+      console.error("❌ Gagal:", err.message);
+  }  
 });
