cf03d7036caaf4d864c939298c0be72800ce7093
wahyuajismustofa <wahyuajismustofa30@gmail.com>
2026-06-07 21:01:30 +0700

    Auto-sync 21:01 07/06/2026

--- assets/js/utils/getEnvironment.js
+++ assets/js/utils/getEnvironment.js
@@ -1,6 +1,6 @@
 const DEV_HOSTS = ["localhost", "127.0.0.1", "wise-hyena-absolutely.ngrok-free.app"];
 const DEV_API = "http://127.0.0.1:8787";
-const PROD_API = "https://api.dekeku.my.id";
+const PROD_API = "https://dekeku-api.wahyuajismustofa.workers.dev";
 
 export function getEnvironment() {
   const host = location.hostname;
